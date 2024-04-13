b390e8dac8c3610f1e1c7263d0feca0d119fba75
Thomson152 <tomsinonyedikachi@gmail.com>
2024-04-13 13:54:40 +0100

    first commit

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-[20vw] text-white h-full  shadow-md' >
+        
+    </div>
+  )
+}
+
+export default Sidebar
